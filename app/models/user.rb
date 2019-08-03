1c87d6265230dea1cebdaa1bcb0fc7ef5e34c8de
cgibanez <cgibanez@tuenti.com>
2019-08-03 23:18:41 +0200

    Added stock listing and removal from portfolio

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,23 @@ class User < ApplicationRecord
 
     has_many :user_stocks
     has_many :stocks, through: :user_stocks
-    
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    def stock_already_added?(ticker_symbol)
+        stock = Stock.find_by_ticker(ticker_symbol)
+        return false unless stock
+        user_stocks.where(stock_id: stock.id).exists?
+    end
+
+    def under_limit?
+        user_stocks.count < 10
+    end
+
+    def can_add_stock?(ticker_symbol)
+        under_limit? and not stock_already_added?(ticker_symbol)
+    end
 end
